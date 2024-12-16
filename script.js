3d311bc30c73abc4cde6a1de1a64071632009dc5
Dulneth210229 <dulneth.sa@gmail.com>
2024-12-16 18:19:31 +0530

    implemented addTodo, saveTodo and render Todo functions

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 // Get todos from localStorage or initialize empty array
-
+let todos = JSON.parse(localStorage.getItem("todos") || []);
 // Function to save todos to localStorage
-
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
 // Function to render todos
+function renderTodos() {}
 
 // Function to add new todo
-
+function addTodo() {
+  const input = document.getElementById("todoInput");
+  const text = input.value.trim();
+  //trim is used to remove white spaces
+  if (text) {
+    todos.push({
+      text: text,
+      completed: false,
+    });
+  }
+  input.value = "";
+}
 // Function to toggle todo completion
 
 // Function to delete todo
